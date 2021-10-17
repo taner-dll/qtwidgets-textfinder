6c02cf2f3bc80f260a5ed19f8c02b17bf532a0e6
Taner Deliloglu <tnrdll@gmail.com>
2021-10-17 21:18:14 +0300

    completed

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,16 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
+#include <QFile>
+#include <QTextStream>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
+
     ui->setupUi(this);
+
+     loadTextFile();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +18,33 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_pushButton_clicked()
+{
+     QString searchString = ui->lineEdit->text();
+     //qDebug() << searchString;
+     ui->textEdit->find(searchString, QTextDocument::FindCaseSensitively);
+
+
+}
+
+
+void MainWindow::loadTextFile()
+{
+    //dosya aç
+//    QFile inputFile(QCoreApplication::applicationDirPath() +"/a.txt");
+    QFile inputFile(":/a.txt");
+    inputFile.open(QIODevice::ReadOnly);
+
+    //read characters from stream
+    QTextStream in(&inputFile);
+    QString lines = in.readAll();
+    //inputFile.close();
+
+    //set textedit lines
+    ui->textEdit->setPlainText(lines);
+
+    //kürsör (imleç) konumunu ayarla
+    QTextCursor cursor = ui->textEdit->textCursor();
+    cursor.movePosition(QTextCursor::Start, QTextCursor::MoveAnchor, 1);
+}
--- mainwindow.h
+++ mainwindow.h
@@ -15,7 +15,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_pushButton_clicked();
+
 private:
     Ui::MainWindow *ui;
+    void loadTextFile();
 };
 #endif // MAINWINDOW_H
